65e80b9acc3fd3bae0b31e8eb1c2d1521be033a4
Hamza Baig <mail.hamzabaig@gmail.com>
2023-09-07 18:07:28 +0500

    Implement functionality to Create New Game using API  and save Game Id returned by API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-import './style.css';
+import "./style.css";
+
+const scoreForm = document.querySelector("#add-score-form");
+const userName = scoreForm.querySelector("#user-name").value;
+const userScore = scoreForm.querySelector("#user-score").value;
+const addScoreBtn = scoreForm.querySelector(".add-score-btn");
+
+async function createNewGame() {
+  const response = await fetch(
+    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: "Code Mode",
+        user: userName,
+        score: userScore,
+      }),
+    }
+  );
+
+  if (response) {
+    const result = await response.json();
+    const startIndex =
+      result.result.indexOf("Game with ID: ") + "Game with ID: ".length;
+    const endIndex = result.result.indexOf(" added.");
+
+    const gameId = result.result.substring(startIndex, endIndex);
+    return gameId;
+  } else {
+    console.error("Request failed with status:", response.status);
+  }
+}
+
+let gameId = await createNewGame();
