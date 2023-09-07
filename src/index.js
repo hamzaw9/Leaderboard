600bcc3b3f152093e32256531f884b9bc9ec02e9
Hamza Baig <mail.hamzabaig@gmail.com>
2023-09-08 00:05:07 +0500

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,37 +1,30 @@
-import "./style.css";
+import './style.css';
 
-const displayScore = document.querySelector("#display-score");
-const scoreForm = document.querySelector("#add-score-form");
-const refreshBtn = document.querySelector(".refresh-btn");
+const displayScore = document.querySelector('#display-score');
+const scoreForm = document.querySelector('#add-score-form');
+const refreshBtn = document.querySelector('.refresh-btn');
 
 const createNewGame = async () => {
-  const response = await fetch(
-    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games",
+  await fetch(
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games',
     {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        name: "Code Mode",
+        name: 'Code Mode',
       }),
-    }
+    },
   );
-
-  if (response) {
-    const result = await response.json();
-  } else {
-    console.error("Request failed with status:", response.status);
-  }
 };
 
 const submitScoreToApi = async (userName, userScore) => {
-  let URL =
-    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/GpRLcTphddAlZCSJitez/scores/";
-  const response = await fetch(URL, {
-    method: "POST",
+  const URL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/GpRLcTphddAlZCSJitez/scores/';
+  await fetch(URL, {
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify({
       user: userName,
@@ -41,33 +34,36 @@ const submitScoreToApi = async (userName, userScore) => {
 };
 
 const fetchScores = async () => {
-  let URL =
-    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/GpRLcTphddAlZCSJitez/scores/";
+  const URL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/GpRLcTphddAlZCSJitez/scores/';
   const response = await fetch(URL);
 
   const data = await response.json();
 
-  displayScore.innerHTML = "";
+  displayScore.innerHTML = '';
   data.result.forEach((e) => {
     displayScore.innerHTML += `<li>${e.user}: ${e.score}</li>`;
   });
 };
 
 const clearFields = () => {
-  const clear = document.querySelectorAll("input");
+  const clear = document.querySelectorAll('input');
   clear.forEach((e) => {
-    e.value = "";
+    e.value = '';
   });
 };
 
-refreshBtn.addEventListener("click", () => {
+refreshBtn.addEventListener('click', () => {
   fetchScores();
 });
 
-document.querySelector("#add-score").addEventListener("click", (event) => {
-  const userName = scoreForm.querySelector("#user-name").value;
-  const userScore = scoreForm.querySelector("#user-score").value;
-  if (event.target.classList.contains("add-score-btn")) {
+window.addEventListener('load', () => {
+  fetchScores();
+});
+
+document.querySelector('#add-score').addEventListener('click', (event) => {
+  const userName = scoreForm.querySelector('#user-name').value;
+  const userScore = scoreForm.querySelector('#user-score').value;
+  if (event.target.classList.contains('add-score-btn')) {
     event.preventDefault();
     submitScoreToApi(userName, userScore);
     clearFields();
