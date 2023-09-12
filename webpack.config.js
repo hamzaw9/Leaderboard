4cb1d7fe3e43945269b5c19b6095d232d431c1a8
Hamza Baig <92848670+hamzaw9@users.noreply.github.com>
2023-09-12 14:59:18 +0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: 'development',
